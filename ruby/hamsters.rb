369e0eeebca618b460a90ae12a26864e0a7b0534
Marla Kallstrom <brandykou@gmail.com>
2017-06-20 15:21:46 -0400

    Add if and until conditionals to prompt proper user entry

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -2,14 +2,26 @@ puts "What is the hamster's name:"
 hamster_name = gets.chomp
 puts "What is the hamster's squeak volume (please use 1-10):"
 volume = gets.chomp.to_i
+  until volume >= 1 && volume <= 10
+    if volume > 10
+      puts "It can't possibly be THAT loud!"
+      volume = gets.chomp.to_i
+    elsif volume <= 0
+      puts "No way it's that quiet. Use a number between 1 and 10."
+      volume = gets.chomp.to_i
+    end
+  end
 puts "What is the hamster's fur color:"
 fur_color = gets.chomp
 puts "Is the hamster a good candidate for adoption (y/n):"
 good_for_adoption = gets.chomp
   if good_for_adoption == "y"
     good_for_adoption = true
-  else
+  elsif good_for_adoption == "n"
     good_for_adoption = false
+  else
+    puts "Hey, I said y/n!! Like, lowercase letters, man."
+    good_for_adoption = gets.chomp
   end
 puts "What is the hamster's estimated age:"
 age = gets.chomp
