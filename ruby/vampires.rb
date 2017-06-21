586bfed0c1745d397c524d4e323c295f6418d60b
Marla Kallstrom <brandykou@gmail.com>
2017-06-20 23:53:10 -0400

    Basic question form

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "Welcome to the team! We're just going to ask you a on-boarding questions. There's no need to be nervous, so just sit back and say whatever comes to mind."
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "OK, and what year were you born?"
+birth_year = gets.chomp
+puts "Our company serves garlic bread, but it's so popular it sells out almost immediately. Should we order some for you?"
+garlic_bread = gets.chomp
+puts "Would you like to enroll in the company health insurance plan?"
+health_insurance = gets.chomp
