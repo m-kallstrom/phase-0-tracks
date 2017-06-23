428f76d322a7bef34d1c8e3191f12d01b747586f
Marla Kallstrom <brandykou@gmail.com>
2017-06-23 19:15:29 -0400

    Fix encrypter to handle Z

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,41 +1,20 @@
 
-=begin
-def encrypter(encrypt_word)
+def encrypter_program(password)
 index = 0
-while index < encrypt_word.length
-  if encrypt_word[index] == "z"
-    puts "a"
-  else
-  encrypt_word[index] = encrypt_word[index].next!
-  puts encrypt_word[index]
-  end
-  index += 1
-end
-end
-
-puts encrypter("abc")
-
-puts encrypter("zed")
-=end
-
-
-def decrypter(decrypt_word)
-  index = 0
-  alphabet = "abcdefghijklmnopqrstuvwxyz"
-  decrypted = ""
-  while index < decrypt_word.length
-    if decrypt_word[index] == "a"
-      decrypted += "z"
+encrypted = ""
+  while index < password.length
+    if password[index] == "z"
+       encrypted += "a"
     else
-      decrypted +=
+       password[index].next!
+       encrypted += password[index]
     end
+    index += 1
   end
-  index += 1
+  puts encrypted
 end
 
-puts decrypter("zzz")
-puts decrypter("zzz")
 
-
-
-#  How can you find out where a letter is in the alphabet, then access the letter right before it? Hint: In the IRB session above, you learned how to ask a string for the index of a letter. "abcdefghijklmnopqrstuvwxyz" counts as a string.
+puts encrypter_program("abc")
+puts encrypter_program("zed")
+puts encrypter_program("swordfish")
