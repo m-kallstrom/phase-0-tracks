515f8e7f327f89235724c7cd6ee0385b891d3fc1
Marla Kallstrom <brandykou@gmail.com>
2017-06-23 22:53:15 -0400

    Add pseudocode and interface

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,40 +1,50 @@
+#METHOD DECLARATIONS:
 
+#Takes in a string and moves each letter forward one position in the alphabet.
 def encrypter_program(password)
-index = 0
-encrypted = ""
-  while index < password.length
-    if password[index] == "z"
-       encrypted += "a"
-    elsif password[index] == " "
-      encrypted += " "
-    else
-       password[index] = password[index].next!
-       encrypted += password[index]
+  index = 0
+  #declare empty variable for encrypted string
+  encrypted = ""
+  #loop through all the string's letters
+    while index < password.length
+      if password[index] == "z"
+         encrypted += "a"
+      elsif password[index] == " "
+        encrypted += " "
+      else
+         password[index] = password[index].next!
+         encrypted += password[index]
+      end
+      index += 1
     end
-    index += 1
-  end
-  p encrypted
+ p encrypted
 end
 
+=begin
 puts encrypter_program("abc")
 puts encrypter_program("zed")
 puts encrypter_program("swordfish")
 puts encrypter_program("secret agent man")
+=end
 
+#Takes in a string and moves each letter back one position in the alphabet.
 def decrypter_program(password)
   index = 0
+  #used for getting a letter's position within the alphabet:
   alphabet = "abcdefghijklmnopqrstuvwxyz"
+  #empty variable for newly decrypted string:
   decrypted = ""
-  new_index = ""
+  #empty variable to calculate new index number to change the letter:
+  new_index_num = ""
+  #loop through all letters in the string:
   while index < password.length
     if password[index] == "a"
       decrypted += "z"
     elsif password[index] == " "
       decrypted += " "
     else
-       new_index = alphabet.index(password[index])
-       new_index = new_index - 1
-       decrypted += alphabet[new_index]
+       new_index_num = alphabet.index(password[index]) - 1
+       decrypted += alphabet[new_index_num]
     end
     index += 1
   end
@@ -49,6 +59,26 @@ puts decrypter_program("tfdsfu bhfou nbo")
 
 
 p decrypter_program(encrypter_program("swordfish"))
-
-# This returns the original string because it first runs the encrypter, which changes each letter forward one position, and then it runs the decrypter, which changes each letter back one position.
+# This returns the original string because it first runs the encrypter, which changes each letter forward one position, and then it runs that result through the decrypter, which changes each letter back one position. But it requires you use p rather than puts!
 =end
+
+
+#DRIVER CODE
+
+#Ask a secret agent whether they would like to decrypt or encrypt a password:
+puts "Greetings, Agent. Which program would you like to run: encryption or decryption?"
+program = gets.chomp.downcase!
+
+#Ask them for the password:
+puts "What is the password you're using?"
+password = gets.chomp.downcase!
+
+#Conducts the requested operation, prints the results to the screen and exits:
+if program == "encryption"
+  puts encrypter_program(password)
+elsif program == "decryption"
+  puts decrypter_program(password)
+else
+  puts "Whoa, there. I thought we hired agents who can type. Try again from the beginning. I ain't putting in another loop for you."
+end
+puts "This program will self destruct in 5...4...3...2...1..."
