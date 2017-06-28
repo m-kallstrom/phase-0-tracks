29b9b323e2fef70a9ca234121104dc859175f9a1
Marla Kallstrom <brandykou@gmail.com>
2017-06-27 23:36:48 -0400

    Add partner's code Release 2 code

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,11 +1,97 @@
 
-def mexican_food
-  mexican_food_1 = "Tacos"
-  mexican_food_2 = "Quesadillas"
-  puts "I love Mexican food!"
-  yield(mexican_food_1, mexican_food_2)
-  puts "Why are you not working?"
+## RELEASE 0
+# if needed for reference
+#
+# def mexican_food
+#   mexican_food_1 = "Tacos"
+#   mexican_food_2 = "Quesadillas"
+#   puts "I love Mexican food!"
+#   yield(mexican_food_1, mexican_food_2)
+#   puts "Why are you not working?"
+# end
+
+#  mexican_food { |mexican_food_1, mexican_food_2| puts "#{mexican_food_1} and #{mexican_food_2} are both great options!" }
+
+#RELEASE 1
+#Declare a hash and an array:
+ mexican_foods_array = ["tacos", "quesadillas", "burritos", "tortilla chips"]
+
+fast_food_joints = {
+ "mcdonalds" => "burgers",
+ "taco_bell" => "mexican",
+ "kfc" => "fried_chicken",
+ "chipotle" => "mexican"
+}
+
+#Check the value of the array before and after running .each
+ p mexican_foods_array
+ mexican_foods_array.each  do |food_type|
+   p "#{food_type} are great!"
+ end
+ p mexican_foods_array
+
+#Check the value of the array during and after running .map!:
+ mexican_foods_array.map! do |food_type|
+   p "#{food_type} are great!"
+ end
+ p mexican_foods_array
+
+#Check the value of the hash before and after running .each:
+p fast_food_joints
+fast_food_joints.each do |restaurant_name, cuisine|
+puts "#{restaurant_name} serves #{cuisine}."
 end
+p fast_food_joints
 
- mexican_food { |mexican_food_1, mexican_food_2| puts "#{mexican_food_1} and #{mexican_food_2} are both great options!" }
+#Check the value of the hash during and after running .map:
+fast_food_joints.map do |restaurant_name, cuisine|
+puts "#{restaurant_name} serves #{cuisine}."
+end
+p fast_food_joints
 
+
+
+##RELEASE 2
+#Declare an array and a hash:
+birthday_party_supplies = ["balloons", "streamers", "cake", "sprinkles", "lots of presents", "guests", "bumpin tunes"]
+
+drinks = {
+  coke: "soda",
+  martini: "cocktail",
+  water: "hydration",
+  coffee: "code",
+  redbull: "wings",
+  tea: "necessity to life",
+  matcha: "heaven"
+}
+
+
+
+ # Using delete_if to get rid of one of the values in the array that doesn't meet the condition
+ birthday_party_supplies.delete_if { |supply| supply.length < 5}
+   puts birthday_party_supplies
+
+## Using delete_if to get rid of one of the values in the has that doesn't meet the condition
+ drinks.delete_if { |beverage, type| type.length < 5}
+   puts drinks
+
+## Keeps only items in the array that meet the condition:
+ birthday_party_supplies.keep_if { |supply| supply.length > 10}
+  puts birthday_party_supplies
+
+##Keeps items in the hash only if they meet the condition:
+ drinks.keep_if { |beverage, type| type.length > 7}
+   puts drinks
+
+## Filter out any items in the array that don't meet a certain condition:
+birthday_party_supplies.reject! { |supply| supply.length < 12}
+  puts birthday_party_supplies
+
+## Filter out any items in the hash that don't meet a certain condition:
+drinks.reject! { |beverage, type| type.length < 9}
+  puts drinks
+
+##Remove items from array until the condition in the block becomes false:
+birthday_party_supplies.select! { |supply| supply.length >= 16}
+
+##Remove items from a hash until the condition in the block becomes false:
