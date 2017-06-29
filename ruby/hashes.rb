627e86add5683b9b2c066e030b022a92d0f2ae61
Marla Kallstrom <brandykou@gmail.com>
2017-06-29 01:47:47 -0400

    Change input to method, fix method for updating key/value

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,49 +1,52 @@
 #Declarations
 new_client = {}
 
+def key_maker(hash, key, value)
+  case
+  when value.include?("yes")
+    value = true
+  when value.include?("no")
+    value = false
+  when value.to_i > 0
+    value = value.to_i
+  else
+    value
+  end
+  hash.store(key.to_sym, value)
+end
 
 #Driver code
 
-#Prompt designer for client information:
-  #-Convert user data to proper type
-  #-Assign to hash
+#Prompt the designer for client information:
+ ## Get input and run it through method
+
 puts "Hello there! Could you give me the client's name?"
 input = gets.chomp
-new_client[:name] = input
+key_maker(new_client, "name", input)
 
 puts "What is the client's age?"
 input = gets.chomp
-new_client[:age] = input.to_i
+key_maker(new_client, "age", input)
 
 puts "What is the client's address?"
 input = gets.chomp
-new_client[:address] = input
+key_maker(new_client, "address", input)
 
 puts "How many children does the client have? (Pets that are treated as children should be counted.)"
 input = gets.chomp
-new_client[:children] = input.to_i
+key_maker(new_client, "children", input)
 
 puts "Describe the client's preferred decor theme:"
 input = gets.chomp
-new_client[:decor] = input
+key_maker(new_client, "decor", input)
 
 puts "Does the client realize their taste is horrible?"
 input = gets.chomp
-if input == "yes" || input == "y"
-  input = true
-else
-  input = false
-end
-new_client[:"Knows Their Taste is Horrible"] = input
+key_maker(new_client, "Taste is horrible", input)
 
-puts "The client doesn't realize that ombre is just an obnoxiously fancy word for 'dip-dying':"
+puts "Does the client seem to realize that ombre is just an obnoxiously fancy word for 'dip-dying'?"
 input = gets.chomp
-if input == "yes" || input == "true"
-  input = true
-else
-  input = false
-end
-new_client[:"Realizes the Word 'Ombre' is Pretentious"] = input
+key_maker(new_client, "Realize ombre sounds pretentious", input)
 
 #Print the data to the screen when questions are finished:
 
@@ -51,41 +54,27 @@ new_client.each do |question, response|
   puts "#{question.to_s.capitalize}: #{response}"
 end
 
-# puts "Name: #{name}"
-# puts "Address: #{address}"
-# puts "Number of Children: #{children}"
-# puts "Preferred Decor Theme: #{decor_theme}"
-# puts "Realize Own Taste is Horrible: #{horrible_taste}"
-# puts "Realize Current Ombre Trend is Pretentious (Even If Pretty): #{ombre_pretentious}"
-
-  #Add labels so that keys remain hidden
 #Give prompt to update key:
 puts "Do you need to make a change?"
 input = gets.chomp
-break
-  input == "no" || input == "none"
+
+#If no updates, enters none and skips to end
+  #-Take in key name and changed value, then
+  #run the key_maker method
+if input == "no" || input == "done"
 else
-  input == "yes" || input == "y"
-  puts "Which category needs to change?"
+  puts "Which category do you need to change?"
   category = gets.chomp
-  puts "What is the new response?"
-  response = gets.chomp
-  new_client[:category.to_sym] = response
+  puts "What should the answer be?"
+  input = gets.chomp
+  key_maker(new_client, category, input)
 end
 
-  #If no updates, enters none and skips to end
-  #-Take in key name and change to symbol
-  #- Take in changed value
 #Print updated version
-# In phase-0-tracks/ruby/hashes.rb, pseudocode and write a program that will allow an interior designer to enter the details of a given client: the client's name, age, number of children, decor theme, and so on (you can choose your own as long as it's a good mix of string, integer, and boolean data).
 
-# Your keys should be symbols unless you find that you need a string for some reason -- usually only when spaces or other "user friendly" formatting are needed (as in the case of using someone's full name as a hash key). Basically, symbols are simpler for programmers to use, and readable enough by technical folks to be used in place of strings most of the time.
+new_client.each do |question, response|
+  puts "#{question.to_s.capitalize}: #{response}"
+end
 
-# Your program should ...
 
-# Prompt the designer/user for all of this information.
-# Convert any user input to the appropriate data type.
-# Print the hash back out to the screen when the designer has answered all of the questions.
-# Give the user the opportunity to update a key (no need to loop, once is fine). After all, sometimes users make mistakes! If the designer says "none", skip it. But if the designer enters "decor_theme" (for example), your program should ask for a new value and update the :decor_theme key. (Hint: Strings have methods that will turn them into symbols, which would be quite handy here.) You can assume the user will correctly input a key that exists in your hash -- no need to handle user errors.
-# Print the latest version of the hash, and exit the program.
-# Be sure to pseudocode, and leave your pseudocode in as comments.
+
