31bb2b5f820b0b344c46b37325653e86e454c73f
Marla Kallstrom <brandykou@gmail.com>
2017-06-30 18:44:02 -0400

    Frame out nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,91 @@
+
+star_wars_saga = {
+  original_trilogy: {
+    A_New_Hope: {
+      number: 4,
+      main_characters: [],
+      release_year: ,
+
+      },
+
+    The_Empire_Strikes_Back: {
+      number: 5,
+      main_characters: [],
+      release_year: ,
+      },
+
+    Return_of_the_Jedi: {
+      number: 6,
+      main_characters: [],
+      release_year: ,
+      },
+    },
+
+  prequel_trilogy: {
+    The_Phantom_Menace: {
+      number: 1,
+      main_characters: [],
+      release_year: ,
+      },
+
+    Attack_of_the_Clones: {
+      number: 2,
+      main_characters: [],
+      release_year: ,
+      },
+
+    Revenge_of_the_Sith: {
+      number: 3,
+      main_characters: [],
+      release_year: ,
+      },
+    },
+
+  sequel_trilogy: {
+    The_Force_Awakens: {
+      number: 7,
+      main_characters: [],
+      release_year: ,
+      },
+
+    The_Last_Jedi: {
+      number: 8,
+      main_characters: [],
+      release_year: ,
+      },
+
+    TBD: {
+      number: 9,
+      main_characters: []
+      release_year: ,
+      },
+    },
+
+  one_shots: {
+    Rogue_One: {
+      time_period: "between 3 and 4",
+      main_characters: [],
+      release_year: ,
+      },
+
+  },
+  cartoons_and_misc: {
+    Star_Wars_Clone_Wars: {
+      time_period: "between 2 and 3",
+      main_characters: [],
+      release_year: ,
+      },
+
+    Star_Wars_Rebels: {
+      time_period: "between 3 and 4 but before Rogue One",
+      main_characters: [],
+      release_year: ,
+      },
+    },
+}
+
+
+
+# In phase-0-tracks/ruby/nested_data_structures.rb, design and build a nested data structure to represent a real-world construct. You can choose the construct: Is it a highway full of cars full of people? A classroom full of desks full of supplies? A fashion show with multiple runways featuring multiple models? Build something that will use a mix of hashes and arrays.
+
+# Once you've built it, print a few individual pieces of deeply nested data from the structure, showing that you know how to use multiple indexes or hash keys (or both) to access nested items. Try to demonstrate a few different types of access.
