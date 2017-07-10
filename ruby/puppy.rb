16a6a379914ad724fdbd442cc12c2ccf83ed615b
Marla Kallstrom <brandykou@gmail.com>
2017-07-10 17:04:44 -0400

    Release 0, add attributes and behaviours

---
+++ ruby/puppy.rb
@@ -0,0 +1,13 @@
+CHARACTERISTICS ----------------
+Ear floppiness: varies
+Friendly: yes
+Cuddleable: yes
+Cuteness scale: 8 minimum
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Snuggle
+Play with toys
+Eat shoes but in a cute way
+Pee on floor and look cutely guilty
