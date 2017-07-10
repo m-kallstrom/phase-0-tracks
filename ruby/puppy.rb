3657cb14c475e4ce8b6528a5598d8048c9377d75
Marla Kallstrom <brandykou@gmail.com>
2017-07-10 17:12:31 -0400

    Add a few more attributes

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -2,12 +2,14 @@ CHARACTERISTICS ----------------
 Ear floppiness: varies
 Friendly: yes
 Cuddleable: yes
-Cuteness scale: 8 minimum
+Cuteness scale 1-10: 8 (minimum)
 Name: varies
 Color: varies
+Toe Bean color: varies
 
 BEHAVIOR -----------------------
 Snuggle
 Play with toys
+Sleep
 Eat shoes but in a cute way
 Pee on floor and look cutely guilty
