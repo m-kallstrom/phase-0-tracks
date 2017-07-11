05da799e876f057aa1e9d60a61b3cc7eacadfbb7
Marla Kallstrom <brandykou@gmail.com>
2017-07-11 01:25:24 -0400

    Additional driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,5 @@
+#METHODS
+
 class Puppy
 
   def initialize
@@ -47,20 +49,43 @@ end
 #DRIVER CODE
 
 #buddy = Puppy.new
-#buddy.fetch("ball")
-#buddy.speak(3)
-#buddy.roll_over
-#puts buddy.dog_years(5)
-#buddy.play_dead
-#sam = Kitty.new
-#sam.speak(3)
-#puts sam.cat_years(5)
+#=>Initializing new puppy instance...
 
+#buddy.fetch("ball")
+#=> I brought back the ball
+
+#buddy.speak(3)
+#=> Woof! Woof! Woof!
+
+#buddy.roll_over
+#=> *rolls over*
+
+#puts buddy.dog_years(5)
+#=> 35
+
+#buddy.play_dead
+#=> Oh no! I am ded.
+#=> *waits five seconds*
+#=> No, I'm not. Give me a biscuit!
+
+#sam = Kitty.new
+#=Initializing new kitty instance...
+
+#sam.speak(3)
+#=> Meow! Meow! Meow!
+
+#puts sam.cat_years(5)
+#=> 30
+
+#Makes 50 new cats
+# (Please spay or neuter your pet!)
+#Updated to pass in a block when initializing
 kitties = {}
 50.times do |x|
   kitties[x] = Kitty.new { puts "New cat" }
 end
 
+#Each of the 50 new cats will meow and tell you how old it is in human years
 kitties.each do |key, value|
   puts key + 1
   value.speak(1)
