9a047290456544d28da90850de88ee1a20af5438
Marla Kallstrom <brandykou@gmail.com>
2017-07-11 02:31:17 -0400

    Release 1, add a block to initialize santas and iterate through hash to print their attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,20 +1,50 @@
 #METHODS
 
 class Santa
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
+
   def speak
     puts "Ho, ho, ho! Haaaaaaaapy Holidays!"
   end
+
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
+  end
+
+  def about
+    puts "#{@gender}"
+    puts "#{@ethnicity}"
+    puts "prefers reindeer in the following order: #{@reindeer_ranking.shuffle}"
+    puts "#{@age} years old"
   end
 end
 
 
 #DRIVER CODE
-#Initialize a new instance of Santa and check its methods are working properly:
-jim = Santa.new
-jim.speak
-jim.eat_milk_and_cookies("Chocolate Chip Cookie")
+#Initialize a new instance of Santa and check its methods are working properly
+# jim = Santa.new("amorphous blob", "cinnamon roll")
+# jim.speak
+# jim.eat_milk_and_cookies("Chocolate Chip Cookie")
+# jim.about
+
+
+example_genders = ["agender", "transmale", "bigender", "male", "female", "transfemale", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latinx", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "Demi-God", "Mystical Creature (Vampire)"]
+
+#Initialize a certain number of Santas and store them in a hash
+santas = {}
+5.times do |santa_id|
+ santas[santa_id] = Santa.new(example_genders.sample, example_ethnicities.sample)
+end
+
+#Iterate through the santas hash and print out their id number followed by their attributes
+santas.each do |id, value|
+ puts "Santa #{id+1} is:"
+ value.about
+end
