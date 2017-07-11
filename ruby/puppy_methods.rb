ebe546ae8245680be1d3dddeb28deb4c38d51e50
Marla Kallstrom <brandykou@gmail.com>
2017-07-10 21:01:38 -0400

    Add pair code for Release 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -28,12 +28,41 @@ class Puppy
   end
 end
 
+class Kitty
+
+  def initialize(&block)
+    yield
+  end
+
+  def speak(number)
+    number.times {puts "Meow!"}
+  end
+
+  def cat_years(human_years)
+    human_years * 6
+  end
+
+end
+
 #DRIVER CODE
 
-buddy = Puppy.new
-buddy.fetch("ball")
-buddy.speak(3)
-buddy.roll_over
-puts buddy.dog_years(5)
-buddy.play_dead
+#buddy = Puppy.new
+#buddy.fetch("ball")
+#buddy.speak(3)
+#buddy.roll_over
+#puts buddy.dog_years(5)
+#buddy.play_dead
+#sam = Kitty.new
+#sam.speak(3)
+#puts sam.cat_years(5)
 
+kitties = {}
+50.times do |x|
+  kitties[x] = Kitty.new { puts "New cat" }
+end
+
+kitties.each do |key, value|
+  puts key + 1
+  value.speak(1)
+  puts value.cat_years(1)
+end
