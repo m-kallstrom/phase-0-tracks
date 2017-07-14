c9f604cc7c70f78388d1ad3db14f6b9719f5b368
Marla Kallstrom <brandykou@gmail.com>
2017-07-14 03:58:31 -0400

    Add classes to module and test

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,8 @@
+
+#Declare module for emphasizing text
+  #declare method for angry emphasis
+  #declare method for happy emphasis
+
 module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
@@ -8,7 +13,25 @@ module Shout
   end
 end
 
-yell = "Ragh"
+#Add a class that would use the shout module angrily
+class Jerk_on_Subway
+  include Shout
+end
 
-p Shout.yell_happily(yell)
-p Shout.yell_angrily(yell)
+#Add a class that would use the shout module happily
+class Friend
+  include Shout
+end
+
+#Initialize some new instances and give them something to say
+speech = "OMG"
+aggressive_hobo = Jerk_on_Subway.new
+new_coding_buddy = Friend.new
+
+#test it works
+p aggressive_hobo.yell_angrily(speech)
+p aggressive_hobo.yell_happily(speech)
+p new_coding_buddy.yell_happily(speech)
+p new_coding_buddy.yell_angrily(speech)
+
+
