363f46502e75f6fa4ff4b4decef5cbf284449127
Marla Kallstrom <brandykou@gmail.com>
2017-07-20 16:30:31 -0400

    add driver code that runs an IF loop for the reverse function

--- js/explore.js
+++ js/explore.js
@@ -17,6 +17,25 @@ function reverseString(string) {
 }
 
 
+var reverseStringFunctionWorks = true;
+var missesRuby = true;
+
+// DRIVER CODE
+
+//declare some true variables
+//IF both conditions are true, print the reverse string
+  // ELSE IF print a different reverse string
+  // ELSE cry
+
+
+if (reverseStringFunctionWorks && missesRuby) {
+  reverseString("Baby, come back! You can blame it all on me!");
+} else if (reverseStringFunctionWorks && !missesRuby) {
+  reverseString("Imma make some sweet JavaScript games!");
+} else {
+  console.log("*cries*");
+}
+
 
 //In phase-0-tracks/js, create a file called explore.js.
 //Ruby has lots of handy functions like .reverse, but in JavaScript, we mostly have to write our own. In comments, pseudocode a function that takes a string as a parameter and reverses the string. reverse("hello"), for example, should return "olleh". This isn't a task specific to JavaScript -- your pseudocode should be in plain English and use phrases like "for each" instead of JavaScript-specific terms. Remember that you can add strings in JavaScript, so '' + 'a' would result in the string 'a'. Take your time. This is the most valuable skill in programming: being able to think through a problem logically.
