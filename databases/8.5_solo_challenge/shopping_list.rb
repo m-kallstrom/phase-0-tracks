ba86005cb1c66468b34a74af9123aff8a3776bab
Marla Kallstrom <brandykou@gmail.com>
2017-07-29 05:03:28 -0400

    add method for printing out appealing-to-humans list of items

--- databases/8.5_solo_challenge/shopping_list.rb
+++ databases/8.5_solo_challenge/shopping_list.rb
@@ -1,7 +1,7 @@
 
 ####Here is the code from the category list maker. I would like it to write to a table or two, and be able to save, retrieve, delete, and print tables.
 require 'sqlite3'
-require 'faker'
+
 #Link necessary files
 #Create two tables for categories and items
   #Category has id and category name
@@ -11,11 +11,12 @@ require 'faker'
   #If it doesn't exist, create it
   #If it does exist, ask if they want to pull it up or overwrite the items
   #Need some security measures in here!
-def make_or_resuse_a_db(database)
-  database += ".db"
-  $DB = SQLite3::Database.new(database)
+#def make_or_resuse_a_db(database)
+ # database += ".db"
+  $DB = SQLite3::Database.new("grocery_list.db")
+ # $DB = SQLite3::Database.new(database)
   $DB.results_as_hash = true
-end
+#end
 
 #Create the tables for category and items
   #send the commands to SQL when called
@@ -41,27 +42,33 @@ def create_item_table
 end
 
 
+#Add category to the database
+def add_categories(category)
+  $DB.execute("INSERT INTO category (name) VALUES (?)", [category])
+end
+
 #Add/Update items
   #Ask user to enter items one at a time, with optional quantity
   #IF the item is already in the list
     #update the table with the new quantity
   #Assuming it's new,
     #Execute a SQL command to stuff that into the items table
-def add_categories(category)
-  $DB.execute("INSERT INTO category (name) VALUES (?)", [category])
+def add_item(category_number, name, quantity=1)
+ return false if $DB.execute("SELECT EXISTS(SELECT 1 FROM items WHERE name=? LIMIT 1)", [name]) == 1
+  # ^^ returns 1 for yes and 0 for no
+  $DB.execute("INSERT INTO items (name, quantity, category_name) VALUES (?, ?, ?)", [name, quantity, category_number])
 end
 
-def add_update_item(category, name, quantity=1)
-
-  $DB.execute("INSERT INTO items (name, quantity, category_name) VALUES (?, ?, ?)", [name, quantity, category])
+def update_item(category_number, name, quantity=1)
+  $DB.execute("UPDATE items SET name =?, quantity =? WHERE category_name=?;", [name, quantity, category_number])
 end
 
 #Look up category number for adding an item
-
+  #select the category information from the database
 def look_up_category_id(input_name)
-
+  categories_from_db = $DB.execute("SELECT * FROM category WHERE name=?", [input_name])
+  categories_from_db[0]["id"]
 end
-
 
 #delete an item
   #ask the user for the item they want to delete
@@ -74,30 +81,48 @@ end
 #Make a user friendly list:
   #Pull the data from the database
   #List numerically by category and then alphabetically by item
-  #Upcase the categories
-  #downcase the items
-
+  #Up case the categories
+  #down case the items
 def pretty_list
-
- items_hash = $DB.execute("SELECT category.name, items.name, items.quantity FROM items INNER JOIN category ON items.category_name = category.id")
- items_hash = items_hash.map {|item, quantity| puts "#{item} is item and quantity is #{quantity}"}
-
+items_list_from_db = $DB.execute("SELECT category.name, items.name, items.quantity FROM category INNER JOIN items ON items.category_name = category.id")
+list = {}
+items_list_from_db.each do |hash|
+    if list.include?(hash[0])
+     list[hash[0]][hash[1]] = hash[2]
+    else
+    item_and_quantity = {}
+    item_and_quantity.store(hash[1], hash[2])
+    list.store(hash[0], item_and_quantity)
+    end
+  end
+  list.each do |key, values|
+    puts "Category: #{key.upcase}"
+    values.each do |item, quantity|
+      puts "#{item.capitalize}: #{quantity}"
+    end
+  end
 end
 
 
 #DRIVER CODE
 #greet user, ask for a list of categories in the general order in which they shop:
-make_or_resuse_a_db("grocery_list")
-$DB.execute(create_category_table)
-$DB.execute(create_item_table)
-add_categories("Fruit and Veg")
-add_categories("Bakery")
-add_categories("Cereals")
-add_categories("Dairy")
-add_update_item(1, "carrots", 5)
-add_update_item(2, "milk", 1)
-delete_item("carrots")
-#p pretty_list
+# make_or_resuse_a_db("grocery_list")
+# $DB.execute(create_category_table)
+# $DB.execute(create_item_table)
+# add_categories("Fruit and Veg")
+# add_categories("Bakery")
+# add_categories("Cereals")
+# add_categories("Dairy")
+# add_update_item(1, "apples", 2)
+# add_update_item(2, "yogurt", 4)
+# delete_item("carrots")
+#pretty_list
+
+p look_up_category_id("dairy")
+
+
+=begin
+
 
 #List set up
   #Ask user for a list of categories in order of how they go around the grocery store
@@ -111,24 +136,67 @@ delete_item("carrots")
         #Delete from old place and insert into new place
     #Repeat until user is happy
     #Iterate through the list to add each to the SQL table
-
 puts "Hello! Please type a name for a new or existing list:"
 puts "(Type carefully because it will create a new list entirely if it's off.)"
 list_name = gets.chomp.downcase
 make_or_resuse_a_db(list_name)
 
-puts "Okay! Let's add some categories to this list. Add them in the order you normally shop through for easiest use:"
+puts "Okay! Let's add some categories to this list. Add them in the order you normally shop through for easiest use. Type 'done' when you're ready:"
 input_category = gets.chomp.downcase
 category_list = []
-loop do
-  break if input_category == "done"
-  category_list << input_category
-  p category_list
-  puts "How's this?"
-  input_category = gets.chomp.downcase
+
+  loop do
+    break if input_category == "done"
+    category_list << input_category
+    p category_list
+    puts "Add another or type 'done':"
+    input_category = gets.chomp.downcase
+  end
+
+#### Pull this stuff out into a method
+puts "Great! Does the order on this look good to you?"
+puts category_list.each_with_index {|category, index| puts "#{index}: #{category}"
+puts "If you want to move an item, type the number. Otherwise, enter 'done':"
+input_number = gets.chomp
+  loop do
+    break if input_number == "done"
+    index = input_number.to_i
+    puts "What number should it be at?"
+    input_number = gets.chomp
+    new_index = input_number.to_i
+
+    puts "How's this? Type 'done' to continue or select another number to change:"
+    input_number = gets.chomp
+  end
+category_list.each {|category| add_categories(category)}
+
+
+#Ask the user to add items one by one
+  #Loop UNTIL they type 'done'
+  #add each item to the table
+  #maybe print the table each time
+puts "Moving right along. Now we've got our categories, let's add some things to them! Put in items by category, the item name and then the quantity (optional)"
+puts "Like this:  frozen pizza 3"
+input_item = gets.chomp.downcase
+
+until input_item == "done"
+  item_array = input_item.split(" ")
+  add_update_item(item_array[0], item_array[1], *item_array[2])
+  puts "Type the next item or 'done' to move on:"
+  input_item = gets.chomp.downcase
 end
 
-category_list.each {|category| add_categories(category)}
+#puts pretty_list
+#puts "How's this look?"
+#puts "Type 'update' to add or update the quantity of the item."
+#input_change_table = gets.chomp.downcase
+
+#put in a loop here for changing
+#puts "Okay. You know the drill now. Category, item name, quantity:"
+#rerun the add_update_item method for any entries.
+
+
+#Maybe it needs to be in a hash as it's being entered, much like the previous version
 
 
 
@@ -252,3 +320,6 @@ puts "Enjoy your shopping trip!"
 ##overwrite puts with def to_s
 
 =end
+
+
+
